e1475ba9cc8529664e9e79e6a743af1054e74857
JerryZhou <JerryZhou@outlook.com>
2015-07-16 16:10:08 +0800

    c++

--- ringbuffer.h
+++ ringbuffer.h
@@ -3,7 +3,19 @@
 
 #include <stdio.h>
 
+/* Set up for C function definitions, even when using C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct iringbuffer; 
+
+typedef enum irbflag {
+    irbflag_blockread = 1,
+    irbflag_blockwrite = 2,
+
+    irbflag_override = 4, 
+}irbflag;
 
 struct iringbuffer *irb_alloc(int capacity, int flag);
 
@@ -14,5 +26,10 @@ int irb_writestr(struct iringbuffer *buffer, const char* str);
 int irb_write(struct iringbuffer *buffer, const char* value, int length);
 
 int irb_read(struct iringbuffer *buffer, char* dst, int length);
+    
+/* Ends C function definitions when using C++ */
+#ifdef __cplusplus
+}
+#endif
 
 #endif
